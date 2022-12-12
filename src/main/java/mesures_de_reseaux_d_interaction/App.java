2e0e75041932f06258ac76dda61d343937ef519d
TEKFA Fouad <fouad.tekfa@etu.univ-lehavre.fr>
2022-12-12 13:56:25 +0100

    ajout de projet mvn

---
+++ src/main/java/mesures_de_reseaux_d_interaction/App.java
@@ -0,0 +1,13 @@
+package mesures_de_reseaux_d_interaction;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/mesures_de_reseaux_d_interaction/AppTest.java
@@ -0,0 +1,20 @@
+package mesures_de_reseaux_d_interaction;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
